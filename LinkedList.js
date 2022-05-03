1ca3e81c949a4e71ad2d54d2f811c33b5dfd0322
Cyprien OTTO <cyprienotto@outlook.fr>
2022-05-04 01:56:55 +0200

    add tail method

--- LinkedList.js
+++ LinkedList.js
@@ -13,6 +13,27 @@ class LinkedList {
       this.head.setNextNode(currentHead);
     }
   }
+
+  addToTail(data) {
+    // Initialy tail is equal to the head of the list
+    let tail = this.head;
+    // check if it has any value
+    if (tail === null) {
+      // If tail has no value, then that means the list was empty
+      const newHead = new Node(data);
+      this.head = newHead; // creating the list’s head
+    }
+    // does tail have a value ? if so,  that means the list is not empty.
+    else {
+      // setting the tail pointer
+      while (tail.getNextNode() !== null) {
+        tail = tail.getNextNode();
+      }
+      // setting the next node tail
+      const newHead = new Node(data);
+      tail.setNextNode(newHead);
+    }
+  }
 }
 
 module.exports = LinkedList;
