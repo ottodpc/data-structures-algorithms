950d946c4952734b961be450123dae80cc8b2160
Cyprien OTTO <cyprienotto@outlook.fr>
2022-05-06 00:03:12 +0200

    add head & tail

--- linear-data-structures/Doubly-Linked-Lists/DoublyLinkedList.js
+++ linear-data-structures/Doubly-Linked-Lists/DoublyLinkedList.js
@@ -1,17 +1,35 @@
 const Node = require('./Node');
 
 class DoublyLinkedList {
-  // Create your constructor below:
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
 
-  printList() {
-    let currentNode = this.head;
-    let output = '<head> ';
-    while (currentNode !== null) {
-      output += currentNode.data + ' ';
-      currentNode = currentNode.getNextNode();
+  addToHead(data) {
+    const newHead = new Node(data);
+    const currentHead = this.head;
+    if (currentHead) {
+      currentHead.setPreviousNode(newHead);
+      newHead.setNextNode(currentHead);
     }
-    output += '<tail>';
-    console.log(output);
+    this.head = newHead;
+    if (!this.tail) {
+      this.tail = newHead;
+    }
+  }
+
+  addToTail(data) {
+    const newTail = new Node(data);
+    const currentTail = this.tail;
+    if (currentTail) {
+      currentTail.setNextNode(newTail);
+      newTail.setPreviousNode(currentTail);
+    }
+    this.tail = newTail;
+    if (!this.head) {
+      this.head = newTail;
+    }
   }
 }
 
