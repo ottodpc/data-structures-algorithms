71ac97b8cc6ac9a7bd1a5293cb7ca112b10666f9
Cyprien OTTO <cyprienotto@outlook.fr>
2022-05-05 06:54:18 +0200

    doubly linked list start

---
+++ linear-data-structures/Doubly-Linked-Lists/DoublyLinkedList.js
@@ -0,0 +1,18 @@
+const Node = require('./Node');
+
+class DoublyLinkedList {
+  // Create your constructor below:
+
+  printList() {
+    let currentNode = this.head;
+    let output = '<head> ';
+    while (currentNode !== null) {
+      output += currentNode.data + ' ';
+      currentNode = currentNode.getNextNode();
+    }
+    output += '<tail>';
+    console.log(output);
+  }
+}
+
+module.exports = DoublyLinkedList;
